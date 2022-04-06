88b62e0acd7f25325a6ae9c91e7f77434e3bd866
Una <contrarian33@gmail.com>
2022-04-06 18:14:10 +0900

    MemberRepository, MemberService

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -21,5 +21,5 @@ public class Member {
     private Address address;
 
     @OneToMany(mappedBy = "member")
-    private List<Order> ordersList = new ArrayList<>();
+    private List<Order> orders = new ArrayList<>();
 }
