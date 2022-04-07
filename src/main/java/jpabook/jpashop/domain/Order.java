237ca85a0a54b099ef18c57a90743a9bc8756a0c
Una <contrarian33@gmail.com>
2022-04-07 16:05:03 +0900

    Order, OrderItem

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -50,4 +50,54 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    // 생성 메서드 존재 이유 :
+    // 주문을 할때 반드시 이 메서드를 통해 주문을 생성하도록 구현하면,
+    // 주문 요건이 변했을때 이 메서드만 수정하면 해결 할 수 있기 때문 (응집력 상승)
+    // 생성 메서드 업이 마구잡이로 Order가 생성된다면 나중에 수정을 해야할 때 어디를 수정해야하는지,
+    // Order 생성에서 버그가 발생했을때 어디가 문제인지 파악하기가 어렵고 시간이 오래걸린다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+//        int totalPrice = 0;
+//        for (OrderItem orderItem : orderItems) {
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
+
 }
